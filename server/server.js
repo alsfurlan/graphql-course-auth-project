254862b3257c29449b56f2f090f191efd52c8c91
Anderson Luís Furlan <alsfurlan@gmail.com>
2023-12-21 02:52:59 -0300

    The User Type

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your Mongo Atlas URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb+srv://alsfurlan:5azCXKlGcE6552eU@cluster0.jm1bqop.mongodb.net/?retryWrites=true&w=majority';
 if (!MONGO_URI) {
   throw new Error('You must provide a Mongo Atlas URI');
 }
