58ec63ccb9bb6ad3b9d9e74285ae890fbc6e8783
Anderson Luís Furlan <alsfurlan@gmail.com>
2023-12-26 19:55:55 -0300

    Authentication state

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
+import query from '../queries/user';
+import { graphql } from 'react-apollo';
 
 class Header extends Component {
   render() {
-    return <div>Header</div>;
+    const { loading, user } = this.props.data;
+    if (loading) return <div />;
+
+    return <div>{user ? 'Logout' : 'Sign Up/Login'}</div>;
   }
 }
 
-export default Header;
+export default graphql(query)(Header);
