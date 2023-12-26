44181c16e62de6bf15ed93dbb1980b73973431b2
Anderson Luís Furlan <alsfurlan@gmail.com>
2023-12-26 16:38:10 -0300

    Root routes with react router

--- client/index.js
+++ client/index.js
@@ -2,6 +2,8 @@ import ApolloClient from 'apollo-client';
 import React from 'react';
 import { ApolloProvider } from 'react-apollo';
 import ReactDOM from 'react-dom';
+import { Router, hashHistory, Route, IndexRoute } from 'react-router';
+import App from './components/App';
 
 const client = new ApolloClient({
   dataIdFromObject: (object) => object.id,
@@ -10,7 +12,9 @@ const client = new ApolloClient({
 const Root = () => {
   return (
     <ApolloProvider client={client}>
-      <div>Auth Starter</div>
+      <Router history={hashHistory}>
+        <Route path='/' component={App}></Route>
+      </Router>
     </ApolloProvider>
   );
 };
