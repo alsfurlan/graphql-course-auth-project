957d1c227dae9b9236ca0500601cf6d0a8947ca2
Anderson Luís Furlan <alsfurlan@gmail.com>
2023-12-27 19:11:33 -0300

    Login and logout buttons

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react';
 import query from '../queries/user';
 import { graphql } from 'react-apollo';
+import { Link } from 'react-router';
 
 class Header extends Component {
-  render() {
+  renderButtons() {
     const { loading, user } = this.props.data;
     if (loading) return <div />;
 
-    return <div>{user ? 'Logout' : 'Sign Up/Login'}</div>;
+    if (user) {
+      return <div>Logout</div>;
+    } else {
+      return (
+        <div>
+          <li>
+            <Link to="/signup">Signup</Link>
+          </li>
+          <li>
+            <Link to="/login">Login</Link>
+          </li>
+        </div>
+      );
+    }
+  }
+
+  render() {
+    return (
+      <nav>
+        <div className='nav-wrapper'>
+          <Link to='/' className='brand-logo left'>
+            Home
+          </Link>
+          <ul className='right'>{this.renderButtons()}</ul>
+        </div>
+      </nav>
+    );
   }
 }
 
