6fd6bbfb2b97dd084afaf13e4e63645f5d76733a
Anderson Luís Furlan <alsfurlan@gmail.com>
2023-12-29 10:01:27 -0300

    Submitting the login form

--- client/components/AuthForm.js
+++ client/components/AuthForm.js
@@ -1,19 +1,39 @@
 import React, { Component } from 'react';
 
 class AuthForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { email: '', password: '' };
+  }
+
+  onSubmit(event) {
+    event.preventDefault();
+    const { email, password } = this.state;
+    this.props.onSubmit(email, password);
+  }
+
   render() {
     return (
-      <div className="row">
-        <form className="col s4">
-          <div className="input-field">
-            <label>E-mail</label>
-            <input />
+      <div className='row'>
+        <form className='col s4' onSubmit={this.onSubmit.bind(this)}>
+          <div className='input-field'>
+            <input
+              placeholder='E-mail'
+              value={this.state.email}
+              onChange={(e) => this.setState({ email: e.target.value })}
+            />
           </div>
-          <div className="input-field">
-            <label>Password</label>
-            <input type="password"/>
+          <div className='input-field'>
+            <input
+              placeholder='Password'
+              type='password'
+              value={this.state.password}
+              onChange={(e) => this.setState({ password: e.target.value })}
+            />
           </div>
-          <button className="btn">Submit</button>
+          <button className='btn' type='submit'>
+            Submit
+          </button>
         </form>
       </div>
     );
--- client/components/LoginForm.js
+++ client/components/LoginForm.js
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
 import AuthForm from './AuthForm';
 import login from '../mutations/login';
+import query from '../queries/user';
 import { graphql } from 'react-apollo';
 
 class LoginForm extends Component {
+  onSubmit(email, password) {
+    this.props.mutate({
+      variables: { email, password },      
+    });
+  }
+
   render() {
     return (
       <div className='container'>
         <h3>Login</h3>
-        <AuthForm />
+        <AuthForm onSubmit={this.onSubmit.bind(this)} />
       </div>
     );
   }
