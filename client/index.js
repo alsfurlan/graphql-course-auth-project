5e56e568701842c4210638bcace41630005abb52
Anderson Luís Furlan <alsfurlan@gmail.com>
2023-12-29 08:15:56 -0300

    Login form design

--- client/index.js
+++ client/index.js
@@ -4,6 +4,7 @@ import { ApolloProvider } from 'react-apollo';
 import ReactDOM from 'react-dom';
 import { Router, hashHistory, Route, IndexRoute } from 'react-router';
 import App from './components/App';
+import LoginForm from './components/LoginForm';
 
 const client = new ApolloClient({
   dataIdFromObject: (object) => object.id,
@@ -13,7 +14,9 @@ const Root = () => {
   return (
     <ApolloProvider client={client}>
       <Router history={hashHistory}>
-        <Route path='/' component={App}></Route>
+        <Route path='/' component={App}>
+          <Route path="/login" component={LoginForm}></Route>
+        </Route>
       </Router>
     </ApolloProvider>
   );
